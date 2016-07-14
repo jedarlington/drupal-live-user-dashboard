e5fd8d6afbaa58f99ccdcf821a86feec1e1feea7
James Drawneek <j.drawneek@ctidigital.com>
2016-07-14 12:12:31 +0100

    Working backbone whoop!

--- js/user-dashboard-app.js
+++ js/user-dashboard-app.js
@@ -1,166 +1,141 @@
 (function ($) {
 
-  // Model
-  var User = Backbone.Model.extend({ });
+    // Model
+    var User = Backbone.Model.extend({ });
 
-  // Collection
-  var Users = Backbone.Collection.extend({
-    model: User,
-    url: 'userdash/data'
-  });
+    // Collection
+    var Users = Backbone.Collection.extend({
+        model: User,
+        url: 'userdash/data'
+    });
 
- //var test = jQuery.getJSON('userdash/data?_format=hal_json');
-
-  // var TestUser = new User({uid: 1});
-  // TestUser.fetch({
-  //   success: function (user) {
-  //     // Check information retrived, could be used directly in a template
-  //     console.log(user.attributes);
-  //     console.log(user.attributes.mail[0].value);
-  //   }
-  // });
-
-    // The View for a Person
-
-
-
-
-    var UserDash = Backbone.View.extend({
-        el: '#userdashboard',
-        initialize: function () {
-            this.render();
-        },
-        render: function () {
-            this.collection.each(function (user) {
-
-                var userView = new UserView({ model: user });
-                this.$el.append(userView.render().el); // calling render method manually.
-
-            }, this);
-            console.log(this);
-            this.$el.html('Hello World');
-            return this; // returning this for chaining..
+    var users = new Users();
+    users.fetch({
+        success: function (collection, response) {
+            var newdash = new DashContainer({collection: collection});
+            newdash.render(collection, response);
         }
     });
 
+
+
+
+    // Lust of Users view.
+    var UserList = Backbone.View.extend({
+        tagName: 'ul',
+        className: 'user-list',
+        events: {
+            'click .reset'      : 'resettheview',
+            'click #show-admin' : 'showadmin'
+        },
+        initialize: function () {
+            _.bindAll(this, 'render');
+        },
+        render: function (collection, response) {
+
+            var element = $(this.el);
+            // Clear potential old entries first
+            element.empty();
+
+            // Loop through all the models and create a userView for each item.
+            _.each(response, function (value, key) {
+                var userView = new UserView({model: value});
+                element.append(userView.render(value).el);
+
+            });
+
+            // Add dashboard controls.
+            var dashcontroller = new DashBoardControls();
+            $(this.el).after(dashcontroller.render().el);
+
+            $('#userdashboard').append(element);
+
+            return this; // returning this for chaining..
+        }
+
+    });
+
     var UserView = Backbone.View.extend({
-        tagNAme: 'li',
-        template: _.template($('#userdashboard').html()),
-        render: function () {
-            this.$el.html( this.template(this.model.toJSON()));
-            this.$el.html('Hello World again');
+        tagName: 'li',
+        className: 'user',
+
+        initialize: function () {
+            _.bindAll(this, 'render');
+        },
+        render: function (model) {
+
+            var element = $(this.el);
+            // Clear potential old entries first
+            element.empty();
+           element.append(_.flatten(_.map(model.name, _.values)));
 
             return this;
         }
     });
 
-
-
-
-    var users = new Users();
-
-    var dashboard = new UserDash({collection: users});
-    //console.log(users.fetch());
-    //console.log(_.pluck(users.fetch(), 'responseText'));
-
-    users.fetch({
-        success : function(collection, response) {
-            console.log(collection);
+    var DashBoardControls = Backbone.View.extend({
+        className: 'controls',
+        initialize: function () {
         },
-
-        error : function(collection, response) {
-            console.log();
+        render: function () {
+            $(this.el).append('<button id="show-admin">Show Admin</button>\n' +
+                '<button class="clear">Clear</button>\n' +
+                '<button class="fetch">Fetch</button>');
+            return this;
         }
     });
 
 
-// The initialize function is always called when instantiating a Backbone View.
-// Consider it the constructor of the class.
+    // Main View that combines all elements.
+    var DashContainer = Backbone.View.extend({
+        tagName: 'div',
+        el: '#userdashboard',
+        events: {
+            'click .clear'      : 'cleartheview',
+            'click #show-admin' : 'showadmin',
+            'click .fetch'      : 'getusers'
+        },
+        initialize: function () {
+            _.bindAll(this, 'render');
+        },
+        render: function (collection, response) {
+
+            var userList = new UserList();
+            this.$el.append(userList.render(collection, response).el);
+
+            var dashcontrol = new DashBoardControls();
+            this.$el.prepend(dashcontrol.render().el);
+
+            var totalusers = _.size(response);
+            $(this.el).prepend('<p>Total number of users: ' + totalusers + ' </p>');
+
+        },
+        cleartheview: function () {
+            $('.user-list').empty();
+        },
+        getusers: function () {
+            users.fetch({
+                success: function (collection, response) {
+                    var userList = new UserList();
+                    userList.render(collection, response).el;
+                }
+            });
+        },
+        showadmin: function () {
+            users.fetch({
+                success: function (collection, response) {
+                    var userList = new UserList();
+
+                    console.log(response);
+                        console.log(_.where(response, {target_id: 'administrator'}));
+                    
+                       // return userList.render(collection, val).el;
 
 
+                }
+            });
+        }
+    });
 
-
-
-
-    //users.add([
-  //  {id: 1, name: 'James'},
-  //  {id: 2, name: 'Craig'}
-  //]);
-  //
-  //console.log(users.get(1));
-  //
-  //var names = users.pluck('name');
-  //
-  //console.log(JSON.stringify(names));
-
-
-
-
-
-
-
-  //// Define the model
-  //User = Backbone.Model.extend();
-  //
-  //// Define the collection
-  //Users = Backbone.Collection.extend({
-  //  model: User,
-  //
-  //  // Url to request when fetch() is called
-  //  url: 'http://drupal8.drupalvm.dev/user/1',
-  //
-  //  parse: function (response) {
-  //    return response.results;
-  //  },
-  //
-  //  // Overwrite the sync method to pass over the Same Origin Policy
-  //  sync: function (method, model, options) {
-  //    var that = this;
-  //
-  //    var params = _.extend({
-  //      type: 'GET',
-  //      dataType: 'json',
-  //      url: that.url,
-  //      processData: false
-  //    }, options);
-  //
-  //    return $.ajax(params);
-  //  }
-  //});
-  //
-  //var users = new Users();
-  //console.log(users);
-  //
-  //// Define the View
-  //UsersView = Backbone.View.extend({
-  //  initialize: function () {
-  //    _.bindAll(this, 'render');
-  //
-  //    // Create a collection
-  //    this.collection = new Users;
-  //
-  //    // Fetch the collection the call render() method
-  //    var that = this;
-  //
-  //    this.collection.fetch({
-  //      success: function () {
-  //        that.render();
-  //      }
-  //    });
-  //  },
-  //
-  //  // Use an external template
-  //  template: _.template($('#usersTemplate').html()),
-  //
-  //  render: function () {
-  //    // Fill the html with the template and the collection
-  //    $(this.el).html(this.template({ users: this.collection.toJSON() }));
-  //  }
-  //});
-  //
-  //var app = new UsersView({
-  //  // Define the el where the view will render
-  //  el: $('body')
-  //});
 
 })(jQuery);
